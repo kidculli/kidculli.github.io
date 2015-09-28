cb41c3465ceac95baab23a0ac2959b9e7063613b
Cullin <Cullin@Cullins-MacBook-Pro.local>
2015-09-28 13:54:16 -0400

    My Webpage links might be broken though

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+    File: main.js
+    Author: Cullin Lam cullin_lam@student.uml.edu
+    UMass Lowell 91.461 GUI Programming I
+    created on: 2015-09-08
+
+    This file serves as a source for javascript code for index.html web page. 
+
+    Currently empty. 
+
+*/ 
